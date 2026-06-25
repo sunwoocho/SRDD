d0f1f283b07da098a88e73428bf4a444f1ae601a
CHO Sunwoo <43809609+sunwoocho@users.noreply.github.com>
2026-06-25 17:00:43 +0900

    Edit training epochs

--- src/3_train_diffusion.sh
+++ src/3_train_diffusion.sh
@@ -1,4 +1,4 @@
 torchrun --nnode=1 --master_port=25678 train_diffusion.py --model DiT-XL/2 \
      --data-path /path/to/your/clustered_data --ckpt pretrained_models/DiT-XL-2-512x512.pt \
-     --global-batch-size 4 --tag minimax --ckpt-every 1000 --log-every 500 --epochs 700 --image-size 512 \
+     --global-batch-size 4 --tag minimax --ckpt-every 1000 --log-every 500 --epochs 70 --image-size 512 \
      --condense --finetune-ipc -1 --results-dir ./path/to/results_dir --spec ost_cluster --nclass 7
